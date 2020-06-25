a2ea1eb7af52cd9b069475fe3ad3bd00d7fcfb74
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-06-24 22:28:12 -0300

    implmentando query com lista de clientes

--- compras/src/main/java/com/springbootgraphql/compras/QueryGraphQL.java
+++ compras/src/main/java/com/springbootgraphql/compras/QueryGraphQL.java
@@ -3,6 +3,9 @@ package com.springbootgraphql.compras;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class QueryGraphQL implements GraphQLQueryResolver {
 
@@ -16,6 +19,14 @@ public class QueryGraphQL implements GraphQLQueryResolver {
 
     public Cliente cliente(){
         return new Cliente("Luiz Felipe", "teste@email.com");
+    }
+
+    public List<Cliente> clientes(){
+        List<Cliente> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++){
+            list.add(new Cliente("Cliente " + i, "teste" + i + "@email.com"));
+        }
+        return list;
     }
 
 }
