bc6f1fe0bea1403c8a1a2255a64bb5416f083b21
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-06-25 14:04:49 -0300

    JPA com a entidade Cliente e refatorando o QueryGraphQL

--- compras/src/main/java/com/springbootgraphql/compras/QueryGraphQL.java
+++ compras/src/main/java/com/springbootgraphql/compras/QueryGraphQL.java
@@ -1,32 +1,31 @@
 package com.springbootgraphql.compras;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class QueryGraphQL implements GraphQLQueryResolver {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     public String hello() {
         return "Hello GraphQL";
     }
 
-    public int soma(int a, int b){
+    public int soma(int a, int b) {
         return a + b;
     }
 
-    public Cliente cliente(){
-        return new Cliente("Luiz Felipe", "teste@email.com");
+    public Cliente cliente(final Long id) {
+        return clienteRepository.findById(id).orElse(null);
     }
 
-    public List<Cliente> clientes(){
-        List<Cliente> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++){
-            list.add(new Cliente("Cliente " + i, "teste" + i + "@email.com"));
-        }
-        return list;
+    public List<Cliente> clientes() {
+        return clienteRepository.findAll();
     }
 
 }
