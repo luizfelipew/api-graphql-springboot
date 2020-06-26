985b1ac7235bbcb2433bf2cb53480ee71de8fe1e
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-06-26 18:16:09 -0300

    salvando e atualizando um cliente

--- compras/src/main/java/com/springbootgraphql/compras/graphql/ClienteGraphQL.java
+++ compras/src/main/java/com/springbootgraphql/compras/graphql/ClienteGraphQL.java
@@ -1,15 +1,17 @@
 package com.springbootgraphql.compras.graphql;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.springbootgraphql.compras.Cliente;
 import com.springbootgraphql.compras.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Component
-public class ClienteGraphQL implements GraphQLQueryResolver {
+public class ClienteGraphQL implements GraphQLQueryResolver, GraphQLMutationResolver {
 
     @Autowired
     private ClienteRepository clienteRepository;
@@ -20,6 +22,15 @@ public class ClienteGraphQL implements GraphQLQueryResolver {
 
     public List<Cliente> clientes() {
         return clienteRepository.findAll();
+    }
+
+    @Transactional
+    public Cliente saveCliente(final Long id, final String nome, final String email) {
+        Cliente cliente = new Cliente();
+        cliente.setId(id);
+        cliente.setNome(nome);
+        cliente.setEmail(email);
+        return clienteRepository.save(cliente);
     }
 
 }
